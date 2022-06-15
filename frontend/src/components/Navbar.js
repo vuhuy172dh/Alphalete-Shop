389e625c32d5129d8d16417441d61e9c215cb040
Vu_Huy <quochuy123dh@gmail.com>
2022-06-16 01:17:21 +0700

    navbar-complete-navbar-top

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,38 +5,37 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="navbar-container">
-      <div className="navbar-info-login">
-        <Swiper modules={[Autoplay]} spaceBetween={100} autoplay={true}>
-          <SwiperSlide>
-            <p>Free Domestic Shipping Over $150 and 30 Day Returns</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p>Women's New Arrivals</p>
-          </SwiperSlide>
-          <SwiperSlide>
-            <p>Men's New Arrivals</p>
-          </SwiperSlide>
-        </Swiper>
-        <ul>
-          <li>Info</li>
-          <li>Login</li>
-        </ul>
-      </div>
-      <div className="navbar-category">
-        <Link to="/" className="navbar-title">
-          ALPHALETE
-        </Link>
-        <ul>
-          <li>MEN</li>
-          <li>WOMEN</li>
-        </ul>
-        <div>
-          <button>look</button>
-          <button>cart</button>
+    <nav className="nav-container">
+      <div className="nav-top-container">
+        <div className="nav-announcement-bar-container">
+          <Swiper modules={[Autoplay]} autoplay={{ delay: 5000 }} loop={true}>
+            <SwiperSlide>
+              <p>Shop Women's New Arrivals</p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p>Shop Men's New Arrivals</p>
+            </SwiperSlide>
+            <SwiperSlide>
+              <p>Free Domestic Shipping Over $150 and 30 Day Returns</p>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+        <div className="nav-info-links-container">
+          <ul className="nav-info-links-list">
+            <li className="nav-info-links-item">
+              <button className="info-btn">Info</button>
+            </li>
+            <li className="nav-info-links-item">
+              <Link to="/account/login" className="login-link">
+                Login
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </div>
+      <hr className="line" />
+      <div className="nav-main-container"></div>
+    </nav>
   );
 }
 
