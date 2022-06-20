5bcd3a67b60aa759fb60f57707847367980b21a8
Vu_Huy <quochuy123dh@gmail.com>
2022-06-20 12:12:32 +0700

    SlideShow is about to be completed

--- frontend/src/components/SlideShow.js
+++ frontend/src/components/SlideShow.js
@@ -1,12 +1,22 @@
 import './SlideShow.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Autoplay } from 'swiper';
+import { Link } from 'react-router-dom';
 
 function SlideShow() {
   return (
     <section>
       <section className="section-slideshow">
         <div className="slideshow-container">
-          <Swiper style={{ cursor: 'grab' }}>
+          <Swiper
+            modules={[Autoplay, Pagination]}
+            style={{ cursor: 'grab' }}
+            autoplay={{ delay: 15000 }}
+            pagination={{
+              type: 'bullets',
+            }}
+            className="mySwiper"
+          >
             <SwiperSlide style={{ width: '940px' }}>
               <div className="title">
                 <div className="title-media-container">
@@ -18,6 +28,86 @@ function SlideShow() {
                     poster="//cdn.shopify.com/s/files/1/0667/0133/files/E527B7F5-53F8-4941-8F3A-AC8079FD6ECC_1400x.jpg?v=1655155189"
                     playsInline={true}
                   ></video>
+                </div>
+                <div className="cta">
+                  <h2 className="cta-title">MEN'S</h2>
+                  <p className="cta-desc">
+                    The next generation of Alphalete sportswear
+                  </p>
+                  <div className="cta-btn-container">
+                    <Link
+                      to="/collections/mens-all-tops"
+                      className="cta-btn-link"
+                    >
+                      <span>ALL TOPS</span>
+                    </Link>
+                    <Link
+                      to="/collections/mens-all-bottoms"
+                      className="cta-btn-link"
+                    >
+                      <span>ALL BOTTOMS</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide style={{ width: '940px' }}>
+              <div className="title">
+                <div className="title-media-container">
+                  <img
+                    src="https://cdn.shopify.com/s/files/1/0667/0133/files/pride-btob-land.jpg?crop=center&v=1654081765&width=1400"
+                    alt="Our favorites"
+                  />
+                </div>
+                <div className="cta">
+                  <h2 className="cta-title">MEN'S</h2>
+                  <p className="cta-desc">
+                    The next generation of Alphalete sportswear
+                  </p>
+                  <div className="cta-btn-container">
+                    <Link
+                      to="/collections/mens-all-tops"
+                      className="cta-btn-link"
+                    >
+                      <span>ALL TOPS</span>
+                    </Link>
+                    <Link
+                      to="/collections/mens-all-bottoms"
+                      className="cta-btn-link"
+                    >
+                      <span>ALL BOTTOMS</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide style={{ width: '940px' }}>
+              <div className="title">
+                <div className="title-media-container">
+                  <img
+                    src="https://cdn.shopify.com/s/files/1/0667/0133/files/AmplifyJune_01-land.jpg?crop=center&v=1654927397&width=1400"
+                    alt="Our favorites"
+                  />
+                </div>
+                <div className="cta">
+                  <h2 className="cta-title">MEN'S</h2>
+                  <p className="cta-desc">
+                    The next generation of Alphalete sportswear
+                  </p>
+                  <div className="cta-btn-container">
+                    <Link
+                      to="/collections/mens-all-tops"
+                      className="cta-btn-link"
+                    >
+                      <span>ALL TOPS</span>
+                    </Link>
+                    <Link
+                      to="/collections/mens-all-bottoms"
+                      className="cta-btn-link"
+                    >
+                      <span>ALL BOTTOMS</span>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </SwiperSlide>
