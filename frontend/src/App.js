bc8842a32a04cf768df5b325a9df1946c055aa7f
Vu_Huy <quochuy123dh@gmail.com>
2022-06-21 09:34:31 +0700

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <header>
+        <Navbar />
+      </header>
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,16 +1,9 @@
-import Navbar from '../components/Navbar';
 import SlideShow from '../components/SlideShow';
 
 function HomePage() {
   return (
     <div>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <SlideShow />
-        <p>hello world</p>
-      </main>
+      <SlideShow />
     </div>
   );
 }
