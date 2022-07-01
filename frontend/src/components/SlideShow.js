e932008bc9dad8ed1211f87fa7def85d6f93b804
Vu_Huy <quochuy123dh@gmail.com>
2022-07-02 00:44:04 +0700

    responsive slideshow
    not complete

--- frontend/src/components/SlideShow.js
+++ frontend/src/components/SlideShow.js
@@ -25,11 +25,19 @@ function SlideShow() {
                 <div className={style.title_media_container}>
                   <video
                     src="https://cdn.shopify.com/s/files/1/0667/0133/files/cg-motivational-land.mp4?v=1654931634"
-                    autoPlay={true}
-                    loop={true}
-                    muted={true}
+                    autoPlay
+                    loop
+                    muted
                     poster="//cdn.shopify.com/s/files/1/0667/0133/files/E527B7F5-53F8-4941-8F3A-AC8079FD6ECC_1400x.jpg?v=1655155189"
-                    playsInline={true}
+                    playsInline
+                  />
+                  <video
+                    src="https://cdn.shopify.com/s/files/1/0667/0133/files/cg-motivational-port.mp4?v=1654931634"
+                    autoPlay
+                    loop
+                    muted
+                    poster="//cdn.shopify.com/s/files/1/0667/0133/files/E527B7F5-53F8-4941-8F3A-AC8079FD6ECC_1400x.jpg?v=1655155189"
+                    playsInline
                   />
                 </div>
                 <div className={style.cta}>
@@ -61,6 +69,10 @@ function SlideShow() {
                     src="https://cdn.shopify.com/s/files/1/0667/0133/files/pride-btob-land.jpg?crop=center&v=1654081765&width=1400"
                     alt="Our favorites"
                   />
+                  <img
+                    src="https://cdn.shopify.com/s/files/1/0667/0133/files/pride-btob-port_800x.jpg?v=1654081769"
+                    alt="Our favorites"
+                  />
                 </div>
                 <div className={style.cta}>
                   <h2 className={style.cta_title}>OUR FAVORITES</h2>
@@ -89,6 +101,10 @@ function SlideShow() {
                 <div className={style.title_media_container}>
                   <img
                     src="https://cdn.shopify.com/s/files/1/0667/0133/files/AmplifyJune_01-land.jpg?crop=center&v=1654927397&width=1400"
+                    alt="Our favorites"
+                  />
+                  <img
+                    src="https://cdn.shopify.com/s/files/1/0667/0133/files/AmplifyJune_01_800x.jpg?v=1654927353"
                     alt="Our favorites"
                   />
                 </div>
