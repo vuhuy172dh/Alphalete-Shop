ed3ae1a6baca9caebf96709c0c776a2c212bdd22
Vu_Huy <quochuy123dh@gmail.com>
2022-07-02 15:26:48 +0700

    add useWindowSize hook
    useWindowSize to take the size of window

---
+++ frontend/src/hooks/useWindowSize.js
@@ -0,0 +1,31 @@
+const { useState, useEffect } = require('react')
+
+function useWindowSize() {
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined
+  })
+
+  useEffect(() => {
+    //handle to call on window resize
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+
+    //add event listener
+    window.addEventListener('resize', handleResize)
+
+    // call handler right away so state get updated with initial window size
+    handleResize()
+
+    //remove event listener on cleanup
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowSize
+}
+
+export default useWindowSize
