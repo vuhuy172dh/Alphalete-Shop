477b701a02bd09a90305f5968516c480d2b05b73
Vu_Huy <quochuy123dh@gmail.com>
2022-07-03 00:02:39 +0700

    navbar drawer updated

--- frontend/src/components/NavDrawer.js
+++ frontend/src/components/NavDrawer.js
@@ -1,9 +1,18 @@
 import style from './NavDrawer.module.css'
 import useWindowSize from '../hooks/useWindowSize'
+import { useState } from 'react'
 
 function NavDrawer(props) {
   const size = useWindowSize()
-  console.log(size.width)
+  const [btnClicked, setBtnClicked] = useState(false)
+
+  function clickMenBtn() {
+    setBtnClicked(false)
+  }
+
+  function clickWomenBtn() {
+    setBtnClicked(true)
+  }
   return (
     <div
       className={
@@ -21,7 +30,34 @@ function NavDrawer(props) {
         }
       ></div>
       <div className={style['navdrawer-container']}>
-        <div className={style['navdrawer-header']}>Hello World</div>
+        <div className={style['navdrawer-header']}>
+          <ul className={style['header-button']}>
+            <li>
+              <button
+                className={
+                  btnClicked === false
+                    ? style['men-btn-active']
+                    : style['men-btn']
+                }
+                onClick={clickMenBtn}
+              >
+                MEN
+              </button>
+            </li>
+            <li>
+              <button
+                className={
+                  btnClicked === true
+                    ? style['women-btn-active']
+                    : style['women-btn']
+                }
+                onClick={clickWomenBtn}
+              >
+                WOMEN
+              </button>
+            </li>
+          </ul>
+        </div>
         <div className={style['navdrawer-scroller']}></div>
       </div>
     </div>
