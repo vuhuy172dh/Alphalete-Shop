2b003415725867e65525d327bd65b5561b998af3
Vu_Huy <quochuy123dh@gmail.com>
2022-07-03 16:10:23 +0700

    update navdrawer
    added navdrawer header/ navdrawer body/ navdrawer footer for men

--- frontend/src/components/NavDrawer.js
+++ frontend/src/components/NavDrawer.js
@@ -1,4 +1,5 @@
 import style from './NavDrawer.module.css'
+import LinkBtn from './LinkBtn'
 import useWindowSize from '../hooks/useWindowSize'
 import { useState } from 'react'
 
@@ -58,7 +59,200 @@ function NavDrawer(props) {
             </li>
           </ul>
         </div>
-        <div className={style['navdrawer-scroller']}></div>
+        <div className={style['navdrawer-scroller']}>
+          <div className={style['navdrawer-body']}>
+            <div
+              id="men-drawer-body"
+              style={{ display: btnClicked === false ? 'flex' : 'none' }}
+              className={style['link-l1-container']}
+            >
+              <ul className={style['link-l1-wrapper']}>
+                <li>
+                  <h2>
+                    <span>Tops</span>
+                  </h2>
+                  <ul className={style['link-l2-wrapper']}>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-stringers"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Stringers</span>
+                      </LinkBtn>
+                    </li>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-tanks"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Tank Tops</span>
+                      </LinkBtn>
+                    </li>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-shirts-tops"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Short Sleeves</span>
+                      </LinkBtn>
+                    </li>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-jackets-hoodies"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Jackets & Hoodies</span>
+                      </LinkBtn>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <h2>
+                    <span>Bottoms</span>
+                  </h2>
+                  <ul className={style['link-l2-wrapper']}>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-shorts"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Shorts</span>
+                      </LinkBtn>
+                    </li>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-joggers"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Joggers</span>
+                      </LinkBtn>
+                    </li>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-boardshorts"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Boardshorts</span>
+                      </LinkBtn>
+                    </li>
+                    <li>
+                      <LinkBtn
+                        link="/collections/mens-base-layers"
+                        color="inherit"
+                        opacity="0.7"
+                        fontweight="600"
+                        fontsize="0.8rem"
+                        paddingtop="2px"
+                        paddingbottom="2px"
+                      >
+                        <span style={{ opacity: 0.7 }}>Base layers</span>
+                      </LinkBtn>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <div className={style['link-l1-poster']}>
+                <div className={style['link-l1-poster-inner']}>
+                  <div className={style['poster-img']}>
+                    <div className={style['poster-img-wrapper']}>
+                      <img
+                        src="https://cdn.shopify.com/s/files/1/0667/0133/files/Elements_85.jpg?crop=center&height=300&v=1648056105&width=250"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                  <div className={style['poster-img']}>
+                    <div className={style['poster-img-wrapper']}>
+                      <img
+                        src="https://cdn.shopify.com/s/files/1/0667/0133/files/Elements_08.jpg?crop=center&height=300&v=1651867703&width=250"
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id="women-drawer-container"></div>
+          </div>
+          <div className={style['navdrawer-footer']}>
+            <div className={style['info-container']}>
+              <ul className={style['info-list']}>
+                <li>
+                  <a href="/login" className={style['info-btn']}>
+                    <i className="fa-solid fa-circle-user" />
+                    <span>Sign In</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/login" className={style['info-btn']}>
+                    <i className="fa-solid fa-truck-fast" />
+                    <span>Shipping Info</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/login" className={style['info-btn']}>
+                    <i className="fa-solid fa-briefcase" />
+                    <span>Careers</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/login" className={style['info-btn']}>
+                    <i className="fa-solid fa-circle-question" />
+                    <span>Help Center</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/login" className={style['info-btn']}>
+                    <i className="fa-solid fa-rotate" />
+                    <span>Returns & Exchange</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="/login" className={style['info-btn']}>
+                    <i className="fa-solid fa-circle-info" />
+                    <span>About Us</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className={style['social-container']}></div>
+          </div>
+        </div>
       </div>
     </div>
   )
